78f2b85e9789c7fa4b564a286430052fc76c1766
yolo <igoncharov@flightpath3d.com>
2024-03-01 15:34:54 +0000

    feat: implement get member-types endpoint

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
 import { graphql } from 'graphql';
+import { rootValue } from './resolvers/hello.js';
+import { log } from 'console';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,7 +15,11 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const { query, variables } = req.body;
+
+      console.log(query);
+
+      return graphql({ schema, source: query, rootValue, variableValues: variables, contextValue: fastify });
     },
   });
 };
--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,6 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLEnumType, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { FastifyInstance } from 'fastify';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +20,43 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+// const rootMutation = new GraphQLObjectType({
+//   name: 'mutation',
+//   fields: {},
+// })
+
+const memberType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: {
+    id: { 
+      type: new GraphQLEnumType({
+        name: "MemberTypeId",
+        values: {
+          BASIC: { value: 'basic' },
+          BUSINESS: { value: 'business' },
+        },
+      }) 
+    },
+    discount: { type: GraphQLInt },
+    postsLimitPerMonth: { type: GraphQLInt },
+  }
+})
+
+const rootQuery = new GraphQLObjectType({
+  name: 'query',
+  fields: {
+    memberTypes: {
+      type: new GraphQLList(memberType),
+      resolve: (source, args, context: FastifyInstance) => {
+        return context.prisma.memberType.findMany();
+      }
+    }
+  },
+})
+
+// Construct a schema, using GraphQL schema language
+export const schema = new GraphQLSchema({
+  query: rootQuery,
+  // mutation: rootMutation,
+})
