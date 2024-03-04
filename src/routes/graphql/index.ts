2f1d21fee8ac83a0dd8085cb9c29e24b706ac237
yolo <igoncharov@flightpath3d.com>
2024-03-04 16:15:42 +0000

    refactor: rm log statement

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -14,9 +14,6 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const { query, variables } = req.body;
-
-      console.log(query);
-
       return graphql({ schema, source: query, variableValues: variables, contextValue: fastify });
     },
   });
